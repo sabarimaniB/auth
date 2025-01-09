3bce468da8c401c8d4d29e0bba2bce612d0ca9ee
sabarimaniB <sabarimani.b2022eee@sece.ac.in>
2025-01-10 01:25:30 +0530

    router

--- auth/src/App.jsx
+++ auth/src/App.jsx
@@ -1,5 +1,14 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
-  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
