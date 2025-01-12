54047c3374e9bfc032bb89144b853ef453de9842
sabarimaniB <sabarimani.b2022eee@sece.ac.in>
2025-01-12 09:42:00 +0530

    server connected

--- auth/src/components/Header.jsx
+++ auth/src/components/Header.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div className='bg-slate-500 text-white'>
-        <div className='flex justify-between items-center max-w-5xl mx-auto p-3'>
+    <div className='bg-slate-400 text-white'>
+        <div className='flex justify-between items-center max-w-5xl mx-auto p-4'>
             <div className='font-bold'><h1>Auth</h1></div>
             <div>
-                <ul className='flex gap-4'>
+                <ul className='flex gap-8 '>
                     <Link to={'/'} >Home</Link>
                     <Link to={'/About'}>About</Link>
                     <Link to={'/Signin'}>Signin</Link>
