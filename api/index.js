47b2c0eba7ac8a4de7dfc55e4f8783fa7c846f83
sabarimaniB <sabarimani.b2022eee@sece.ac.in>
2025-02-01 21:13:05 +0530

    user model

--- api/index.js
+++ api/index.js
@@ -5,6 +5,23 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.listen(3000, () => {
-  console.log("Server Running on PORT 3000");
+const PORT = process.env.PORT || 3000; 
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/sabari");
+    console.log("Connected to MongoDB");
+    console.log("Database Name:", mongoose.connection.name);
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+};
+
+app.listen(PORT, () => {
+  console.log(`Server Running on PORT ${PORT}!`);
+  connectToMongoDB();
 });
